e4fadd41c07b27846dff827ca46cc871195ca428
johnrobertmahlan <johnrobertmahlan@gmail.com>
2021-02-11 18:51:22 -0700

    implement initial create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
-    def new
-        @task = Task.new
+    # def new
+    #     @task = Task.new
+    # end
+
+    def index
+        @tasks = Task.all
+    end
+
+    def create
+        @task = Task.create(task_params)
+        puts  "A new record has been created"
+        @task.save
+        redirect_to root_path
+    end
+
+    private
+    def task_params
+        params.permit(:title)
     end
 end
